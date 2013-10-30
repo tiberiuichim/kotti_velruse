0e95c8286473fecea4e77f010f1fc5ba2b607c55
Richard Gomes <rgomes.info@gmail.com>
2013-10-30 11:48:49 +0000

    code review

--- kotti_velruse/__init__.py
+++ kotti_velruse/__init__.py
@@ -9,34 +9,4 @@ _ = TranslationStringFactory('kotti_velruse')
 
 def kotti_configure(settings):
     settings['pyramid.includes'] += ' velruse.app'
-    settings['pyramid.includes'] += ' kotti_velruse'
-    #-- settings['kotti.populators'] += ' kotti_velruse.populate.populate_settings'
-
-
-def includeme(config):
-    import views
-
-    config.add_view(views.login,
-                    route_name='login',
-                    request_method='GET',
-                    renderer='kotti_velruse:templates/login.mako')
-    config.add_view(views.login_,
-                    route_name='login_',
-                    renderer='json')
-    config.add_view(views.logged_in,
-                    route_name='logged_in',
-                    renderer='json')
-    config.add_view(views.logout,
-                    route_name='logout',
-                    permission='view')
-
-    config.add_route('login',     '/login')
-    config.add_route('login_',    '/login_')
-    config.add_route('logged_in', '/logged_in')
-    config.add_route('logout',    '/logout')
-
-    import openid_selector
-    config.add_static_view(name='js',     path='openid_selector:/js')
-    config.add_static_view(name='css',    path='openid_selector:/css')
-    config.add_static_view(name='images', path='openid_selector:/images')
-    
+    settings['pyramid.includes'] += ' kotti_velruse.views'
--- kotti_velruse/views.py
+++ kotti_velruse/views.py
@@ -9,6 +9,38 @@ from kotti_velruse.events import before_kotti_velruse_loggedin, before_kotti_vel
                                  after_kotti_velruse_loggedin,  after_kotti_velruse_loggedout
 
 
+def includeme(config):
+    config.add_view(login,
+                    route_name='login',
+                    request_method='GET',
+                    renderer='kotti_velruse:templates/login.mako')
+    config.add_view(login_,
+                    route_name='login_',
+                    renderer='json')
+    config.add_view(logged_in,
+                    route_name='logged_in',
+                    renderer='json')
+    config.add_view(logout,
+                    route_name='logout',
+                    permission='view')
+
+    config.add_route('login',     '/login')
+    config.add_route('login_',    '/login_')
+    config.add_route('logged_in', '/logged_in')
+    config.add_route('logout',    '/logout')
+
+    try:
+        import openid_selector
+        log.info('openid_selector loaded successfully')
+        config.add_static_view(name='js',     path='openid_selector:/js')
+        config.add_static_view(name='css',    path='openid_selector:/css')
+        config.add_static_view(name='images', path='openid_selector:/images')
+    except Exception as e:
+        log.error(e)
+        raise e
+    log.info('kotti_velruse views are configured.')
+    
+
 def login(request):
     settings = request.registry.settings
     try:
@@ -46,7 +78,7 @@ def login_(request):
 
     settings = request.registry.settings
     if not method in find_providers(settings):
-        raise HTTPNotFound('Provider {}({}) is not configured'.format(provider, method)).exception
+        raise HTTPNotFound('Provider/method {}/{} is not configured'.format(provider, method)).exception
 
     velruse_url = login_url(request, method)
 
@@ -64,7 +96,7 @@ def login_(request):
         return response
     except Exception as e:
         log.error(e.message)
-        message = _(u'Provider:{} Method:{} :: {}').format(provider, method, e.message)
+        message = _(u'Provider/method: {}/{} :: {}').format(provider, method, e.message)
         raise HTTPNotFound(message).exception
 
 
@@ -78,10 +110,7 @@ def logged_in(request):
         return json
     except Exception as e:
         log.error(e.message)
-        provider='?'
-        method='?'
-        message = _(u'Provider:{} Method:{} :: {}').format(provider, method, e.message)
-        raise HTTPNotFound(message).exception
+        raise HTTPNotFound(e.message).exception
 
 
 def logout(request):
